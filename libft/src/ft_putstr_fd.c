b14e9a4f1829254667063608731e523646aa11be
jpceia <joao.p.ceia@gmail.com>
2021-10-08 09:41:25 +0100

    added (void)! prefix to avoid warning warn_unused_result

--- libft/src/ft_putstr_fd.c
+++ libft/src/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 21:39:21 by jpceia            #+#    #+#             */
-/*   Updated: 2021/02/08 21:39:36 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/10/08 09:01:32 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	(void)!write(fd, s, ft_strlen(s));
 }
