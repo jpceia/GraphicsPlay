c3939b1be83646e552ef97738c59e28ac1394677
jpceia <joao.p.ceia@gmail.com>
2021-10-14 18:38:39 +0100

    added fclipf to color.c

--- src/color.c
+++ src/color.c
@@ -6,19 +6,26 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 02:53:52 by jceia             #+#    #+#             */
-/*   Updated: 2021/09/13 20:08:29 by jceia            ###   ########.fr       */
+/*   Updated: 2021/10/14 18:32:16 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+#include <math.h>
+
+float	fclipf(float x, float min, float max)
+{
+	return (fminf(fmaxf(x, min), max));
+}
 
 int	create_trgb(t_rgb v)
 {
 	return (
 		(0) << 24
-		| ((int)(255 * v.x)) << 16
-		| ((int)(255 * v.y)) << 8
-		| ((int)(255 * v.z)));
+		| ((int)(255 * fclipf(v.x, 0, 1))) << 16
+		| ((int)(255 * fclipf(v.y, 0, 1))) << 8
+		| ((int)(255 * fclipf(v.z, 0, 1)))
+	);
 }
 
 int	get_t(int trgb)
