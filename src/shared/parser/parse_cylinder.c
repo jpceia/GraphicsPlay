f06b680dcc1582f8061467f2e86b136c78b0c2fd
jpceia <joao.p.ceia@gmail.com>
2021-10-20 16:19:44 +0100

    moved files to mandatory / shared / bonus folders, to divide the project into mandaroty and bonus

---
+++ src/shared/parser/parse_cylinder.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_cylinder.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 16:05:28 by jceia             #+#    #+#             */
+/*   Updated: 2021/10/20 16:05:47 by jceia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+t_object	*parse_cylinder_from_line(t_object *obj, char *line)
+{
+	t_cylinder	*cylinder;
+	char		**s_split;
+	int			n;
+
+	obj->obj_type = CYLINDER;
+	n = ft_strwc(line, ' ');
+	if (n != 6)
+		return (clean_exit(NULL, "Line with incorrect format", NULL, 0));
+	cylinder = (t_cylinder *)malloc(sizeof(*cylinder));
+	if (!cylinder)
+		return (clean_exit(NULL, "Error allocating memory", NULL, 0));
+	s_split = ft_split(line, ' ');
+	if (!s_split)
+		return (clean_exit(cylinder, "Error spliting line", free, 0));
+	if (!parse_vec3d(&cylinder->p, s_split[1]))
+		return (clean_exit(cylinder, "Error parsing vector", free, 0));
+	if (!parse_vec3d(&cylinder->direction, s_split[2]))
+		return (clean_exit(cylinder, "Error parsing vector", free, 0));
+	cylinder->direction = vec3d_normalize(cylinder->direction);
+	cylinder->radius = ft_atof(s_split[3]) / 2;
+	cylinder->height = ft_atof(s_split[4]);
+	if (!parse_color(&cylinder->color, s_split[5]))
+		return (clean_exit(cylinder, "Error parsing rgb color", free, 0));
+	ft_str_array_clear(s_split, n);
+	obj->data = cylinder;
+	return (obj);
+}
---
+++ src/shared/parser/parse_sphere.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_sphere.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 16:05:57 by jceia             #+#    #+#             */
+/*   Updated: 2021/10/20 16:06:05 by jceia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+t_object	*parse_sphere_from_line(t_object *obj, char *line)
+{
+	t_sphere	*sphere;
+	char		**s_split;
+	int			n;
+
+	obj->obj_type = SPHERE;
+	n = ft_strwc(line, ' ');
+	if (n != 4)
+		return (clean_exit(NULL, "Line with incorrect format", NULL, 0));
+	sphere = (t_sphere *)malloc(sizeof(*sphere));
+	if (!sphere)
+		return (clean_exit(NULL, "Error allocating memory", NULL, 0));
+	s_split = ft_split(line, ' ');
+	if (!s_split)
+		return (clean_exit(sphere, "Error spliting line", free, 0));
+	if (!parse_vec3d(&sphere->center, s_split[1]))
+		return (clean_exit(sphere, "Error parsing vector", free, 0));
+	sphere->radius = ft_atof(s_split[2]) / 2;
+	if (!parse_color(&sphere->color, s_split[3]))
+		return (clean_exit(sphere, "Error parsing rgb color", free, 0));
+	ft_str_array_clear(s_split, n);
+	obj->data = sphere;
+	return (obj);
+}
