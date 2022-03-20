9aeb0b4f1f71ff897ee2590cafdee0c8948ea3d7
João Ceia <joao.p.ceia@gmail.com>
2022-03-20 12:09:13 +0000

    added smooth_x and smooth_y functions

--- headers/gradient.hpp
+++ headers/gradient.hpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 02:39:13 by jpceia            #+#    #+#             */
-/*   Updated: 2022/03/14 12:30:01 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/03/20 12:08:59 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,42 @@ void gradient_y(T* M, int width, int height, T* res)
             res[k + j] = (M[k + j + width] - M[k + j - width]) / 2.0;
 }
 
+template <typename T>
+void smooth_x(T* M, int width, int height)
+{
+    for (int j = 0; j < height; ++j) // for each row
+    {
+        T curr = M[j * width];
+        T next = M[j * width + 1];
+        M[j * width] = (0.5 * curr + 0.25 * next) / 0.75;
+        for (int i = 1; i < width - 1; ++i)
+        {
+            T prev = curr;
+            curr = next;
+            next = M[j * width + i + 1];
+            M[j * width + i] = 0.25 * prev + 0.5 * curr + 0.25 * next;
+        }
+        M[j * width + width - 1] = (0.25 * curr + 0.5 * next) / 0.75;
+    }
+}
+
+template <typename T>
+void smooth_y(T* M, int width, int height)
+{
+    for (int i = 0; i < height; ++i) // for each column
+    {
+        T curr = M[i * height];
+        T next = M[(i + 1) * height];
+        M[i * height] = (0.5 * curr + 0.25 * next) / 0.75;
+        for (int j = 1; j < width - 1; ++j)
+        {
+            T prev = curr;
+            curr = next;
+            next = M[j * width + i + 1];
+            M[j * width + i] = 0.25 * prev + 0.5 * curr + 0.25 * next;
+        }
+        M[i * width + width - 1] = (0.25 * curr + 0.5 * next) / 0.75;
+    }
+}
 
 #endif
